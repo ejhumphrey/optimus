0438ed93e5edc54b4b11e5794c0f0f749992318b
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-04-06 23:35:52 -0400

    Refactored Factory to Queue, updated names ... all is in flux

--- data/queue.py
+++ data/queue.py
@@ -3,10 +3,72 @@
 
 from . import sources
 from . import selectors
+import numpy as np
 
 
-class Factory(object):
-    """writeme"""
+class LocalCache(object):
+    """Basically, a dictionary with some bonus methods.
+    """
+    def __init__(self, refresh_prob, selector=None):
+        """writeme."""
+
+        if selector is None:
+            selector = selectors.permute_items
+        self._selector = selector
+
+        self._data = dict()
+        self._refresh_prob = refresh_prob
+        self._item_gen = self._selector(self._data)
+
+    def get(self, key):
+        """
+        Fetch the Group for a given key.
+        """
+        return self._data.get(key)
+
+    def add(self, key, entity):
+        """writeme."""
+        self._data[key] = entity
+        self._item_gen = self._selector(self._data)
+
+    def keys(self):
+        """writeme."""
+        return self._data.keys()
+
+    def __len__(self):
+        return len(self._data)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def remove(self, key, prob=1):
+        """Swap an existing key-value pair with a new one."""
+        # Refresh on success.
+        if np.random.binomial(1, p=prob):
+            print "Deleting %s" % key
+            self.remove(key)
+
+    def next(self):
+        """Return the next item."""
+        key, entity = self._item_gen.next()
+        self.remove(key, prob=self._refresh_prob)
+        return key, entity
+
+
+class Queue(object):
+    """
+    The Queue Pipeline:
+
+    Source
+      |
+    Selector
+      |
+    Transformer
+      |
+    Buffer
+
+
+    """
     def __init__(self, source, batch_size, transformers=None, selector=None,
                  cache_size=1000, refresh_prob=0.01):
         """writeme"""
@@ -29,8 +91,7 @@ class Factory(object):
             cache_size = len(source)
             refresh_prob = 0
 
-        self.cache = sources.LocalCache(
-            refresh_prob=refresh_prob, selector=selector)
+        self.cache = LocalCache(refresh_prob=refresh_prob, selector=selector)
         self._cache_size = cache_size
         self.populate()
         self.update()
