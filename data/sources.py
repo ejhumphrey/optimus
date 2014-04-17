c6e44d563337ed7665e166db9a032a3f295b4b78
Eric J. Humphrey <ejhumphrey@nyu.edu>
2014-04-16 22:32:13 -0400

    Major changes to internal keymap structure, but all tests pass so fuckya

--- data/sources.py
+++ data/sources.py
@@ -1,8 +1,9 @@
 """
 """
-import time
+#import time
 import h5py
-from collections import OrderedDict
+import json
+import numpy as np
 
 from . import core
 from . import keyutils
@@ -10,25 +11,26 @@ from . import keyutils
 
 class File(h5py.File):
     """writeme."""
-    __KEYS__ = "__KEYS__"
-    __ADDR__ = "__ADDR__"
+    __KEYMAP__ = "__KEYMAP__"
     __WIDTH__ = 256
     __DEPTH__ = 3
 
     def __init__(self, name, mode=None, entity=None, **kwds):
         """writeme."""
         h5py.File.__init__(self, name=name, mode=mode, **kwds)
-        keys, addrs = [], []
 
         if entity is None:
             self._entity_cls = core.Entity
-        if self.__KEYS__ in self:
-            keys = self[self.__KEYS__]
-        if self.__ADDR__ in self:
-            addrs = self[self.__ADDR__]
 
-        self._key_map = OrderedDict([(k, a) for k, a in zip(keys, addrs)])
+        self._keymap = self.__decode_keymap__()
         self._agu = keyutils.uniform_hexgen(self.__DEPTH__, self.__WIDTH__)
+
+    def __decode_keymap__(self):
+        keymap_array = np.array(h5py.File.get(self, self.__KEYMAP__, '{}'))
+        return json.loads(keymap_array.tostring())
+
+    def __encode_keymap__(self, keymap):
+        return np.array(json.dumps(keymap))
 
     def __del__(self):
         """Safe default destructor"""
@@ -37,22 +39,19 @@ class File(h5py.File):
 
     def close(self):
         """write keys and paths to disk"""
-        if self.__KEYS__ in self:
-            del self[self.__KEYS__]
+        if self.__KEYMAP__ in self:
+            del self[self.__KEYMAP__]
+        keymap_str = self.__encode_keymap__(self._keymap)
         h5py.File.create_dataset(
-            self, name=self.__KEYS__, data=self._key_map.keys())
+            self, name=self.__KEYMAP__, data=keymap_str)
 
-        if self.__ADDR__ in self:
-            del self[self.__ADDR__]
-        h5py.File.create_dataset(
-            self, name=self.__ADDR__, data=self._key_map.values())
         h5py.File.close(self)
 
     def get(self, key):
         """
         Fetch the Group for a given key.
         """
-        addr = self._key_map.get(key)
+        addr = self._keymap.get(key)
         raw_group = h5py.File.get(self, addr)
         raw_key = raw_group.attrs.get("key")
         assert raw_key == key, \
@@ -62,14 +61,14 @@ class File(h5py.File):
     def add(self, key, entity):
         """writeme."""
         key = str(key)
-        assert not key in self._key_map
+        assert not key in self._keymap
         addr = self._agu.next()
         while addr in self:
             addr = self._agu.next()
 
         # print "%s -> %s" % (key, addr)
 
-        self._key_map[key] = addr
+        self._keymap[key] = addr
         new_grp = self.create_group(addr)
         new_grp.attrs.create(name='key', data=key)
         for dset_key, dset in dict(**entity).iteritems():
@@ -79,11 +78,11 @@ class File(h5py.File):
 
     def keys(self):
         """writeme."""
-        return self._key_map.keys()
+        return self._keymap.keys()
 
     def paths(self):
         """writeme."""
-        return self._key_map.values()
+        return self._keymap.values()
 
     def __len__(self):
         return len(self.keys())
