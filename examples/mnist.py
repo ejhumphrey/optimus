794591a3cb40a74c6d13fd37fb53d133d822f322
Eric J. Humphrey <ejhumphrey@nyu.edu>
2015-01-04 00:00:06 -0500

    MNIST demo script is reasonably complete

--- examples/mnist.py
+++ examples/mnist.py
@@ -1,6 +1,20 @@
 """Demonstration of building an MNIST classifier.
+
+
+This script and its corresponding functionality expects the gzipped pickle file
+provided courtesy of the Theano dev team at Montreal:
+
+    http://deeplearning.net/data/mnist/mnist.pkl.gz
+
+
+Sample call:
+python ./examples/mnist.py \
+~/mnist.pkl.gz \
+./examples/sample_definition.json \
+examples/sample_params.npz
 """
 
+import argparse
 import cPickle
 import gzip
 import numpy as np
@@ -8,15 +22,12 @@ import numpy as np
 import optimus
 
 
-def load_mnist(gzfile):
+def load_mnist(mnist_file):
     """Load the MNIST dataset into memory.
-
-    Expects the pre-processed data provided courtesy of the Theano devs:
-        http://deeplearning.net/data/mnist/mnist.pkl.gz
 
     Parameters
     ----------
-    gzfile : str
+    mnist_file : str
         Path to gzipped MNIST file.
 
     Returns
@@ -26,7 +37,7 @@ def load_mnist(gzfile):
         labels.shape=(N,).
     """
     dsets = []
-    with gzip.open(gzfile, 'rb') as fp:
+    with gzip.open(mnist_file, 'rb') as fp:
         for split in cPickle.load(fp):
             n_samples = len(split[1])
             data = np.zeros([n_samples, 1, 28, 28])
@@ -81,7 +92,41 @@ def minibatch(data, labels, batch_size, max_iter=np.inf):
         count += 1
 
 
-def build_model():
+def build_models():
+    """Create a trainer and predictor graph for digit classification.
+
+    Returns
+    -------
+    trainer : optimus.Graph
+        Graph with in-place updates for back-propagating error.
+
+        Parameters
+        ----------
+        data : np.ndarray, shape=(*, 1, 28, 28)
+            Images to process.
+        labels : np.ndarray, shape=(*,), dtype=int
+            Target class indices of `data`.
+        learning_rate : scalar, > 0
+            Update parameter.
+
+        Returns
+        -------
+        result : dict
+            Requested outputs; keyed under 'loss' and 'likelihoods'.
+
+    predictor : optimus.Graph
+        Static graph for processing images.
+
+        Parameters
+        ----------
+        data : np.ndarray, shape=(*, 1, 28, 28)
+            Images to process.
+
+        Returns
+        -------
+        result : dict
+            Requested outputs; keyed under 'likelihoods'.
+    """
     # 1.1 Create Inputs
     input_data = optimus.Input(
         name='data',
@@ -173,15 +218,45 @@ def build_model():
     return trainer, predictor
 
 
-def main(mnist_file):
-    # Create the models and driver
-    trainer, predictor = build_model()
-    driver = optimus.Driver(graph=trainer, name='example_classifier')
+def main(args):
+    # Create the models, and pass the trainer to a driver.
+    trainer, predictor = build_models()
+    driver = optimus.Driver(graph=trainer)
 
     # Load data and configure the minibatch generator.
-    train, valid, test = load_mnist(mnist_file)
-    stream = minibatch(train[0], train[1], 20)
+    train, valid, test = load_mnist(args.mnist_file)
+    stream = minibatch(train[0], train[1], 50)
     hyperparams = dict(learning_rate=0.02)
 
     # And we're off!
     driver.fit(stream, hyperparams=hyperparams, max_iter=500, print_freq=20)
+
+    # Note that, because they were made at the same time, `trainer` and
+    #   `predictor` share the same parameters.
+    y_pred = predictor(valid[0])['likelihoods'].argmax(axis=1)
+    print "Accuracy: {0:0.4}".format(np.equal(valid[1], y_pred).mean())
+
+    # Now that it's done, save the graph and parameters to disk.
+    optimus.save(predictor, args.def_file, args.param_file)
+
+    # And, because we can, let's recreate it purely from disk.
+    predictor2 = optimus.load(args.def_file, args.param_file)
+    y_pred2 = predictor2(valid[0])['likelihoods'].argmax(axis=1)
+    print "Consistency: {0:0.4}".format(np.equal(y_pred, y_pred2).mean())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+
+    # Inputs
+    parser.add_argument("mnist_file",
+                        metavar="mnist_file", type=str,
+                        help="Path to the MNIST data file.")
+    # Outputs
+    parser.add_argument("def_file",
+                        metavar="def_file", type=str,
+                        help="Path to save the classifier graph.")
+    parser.add_argument("param_file",
+                        metavar="param_file", type=str,
+                        help="Path to save the classifier parameters.")
+    main(parser.parse_args())
